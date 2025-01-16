45b22d7bacf7582c44fa20d47a341100ff00d43c
Andrzej <149208469+lu0i@users.noreply.github.com>
2025-01-16 14:57:20 +0100

    Create Klient.cs

---
+++ Klient.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hurtowniaGenerowanieFaktur
+{
+    public class Klient
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Adres { get; set; }   
+    }
+}
